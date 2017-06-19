52d3ec1314cb3bc5e72bc1e3a56be44d83079fe5
ubihub <ubihub@gmail.com>
2017-06-18 23:00:06 -0700

    update extend class

--- src/ca/bcitsa/android/PhoneList.java
+++ src/ca/bcitsa/android/PhoneList.java
@@ -14,16 +14,16 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class PhoneList extends Activity {
+public class PhoneList extends FragmentActivity {
 
 	org.w3c.dom.Document doc;
 	ListView listview;;
